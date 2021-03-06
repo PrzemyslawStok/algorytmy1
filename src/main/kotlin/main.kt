9d82ee4986c8b826664680c78ec2dafdc5ce834d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-03-06 15:14:09 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,42 @@
-fun main() {
-    val tab = arrayOf(1,2,3,4,5,6,7,8,9,10,2,2)
-    println(tab.asList())
+import kotlin.random.Random
 
-    val set = setOf(1,2,3,4,5,6,7,8,9,10,2,2)
+fun main() {
+    val tab = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    println(tab)
+
+    val set = mutableSetOf(1, 2, 10, 2, 2)
     println(set)
+
+    //1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4,5,5,5,5,5
+
+    for (i in 1..5) {
+        for (j in 1..5) {
+            tab.add(i)
+            set.add(i)
+        }
+    }
+
+    println(tab)
+    println(set)
+
+    val no = Random.nextInt(1, 10)
+
+    for (i in 1..10)
+        tab.add(Random.nextInt(0, 2))
+
+    println(tab)
+
+    set.addAll(tab)
+
+    println(set)
+
+    val tab1 = tab.filter { it % 2 == 0 }
+
+    println(tab1)
+
+
+    tab1.forEach { println("element: ${it}") }
+    println(tab1.map { it * it })
+
+
 }
